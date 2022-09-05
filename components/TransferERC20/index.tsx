f96eb1dfe7e0b7111d4db1f5cce56b4778cb22e6
simon-tanna <gcab022109@coderacademy.edu.au>
2022-09-05 15:40:28 +1000

    implemented withdrawal logic in form field contained in newly created TransferERC20 component

--- components/TransferERC20/index.tsx
+++ components/TransferERC20/index.tsx
@@ -9,3 +9,48 @@ import {
 	FormLabel,
 } from "@chakra-ui/react";
 
+interface Props {
+	addressContract: string;
+	currentAccount: string | undefined;
+}
+
+export default function TransferERC20(props: Props) {
+	const addressContract = props.addressContract;
+	const currentAccount = props.currentAccount;
+	const [amount, setAmount] = useState<string>("100");
+	const [toAddress, setToAddress] = useState<string>("");
+
+	async function submitTransfer(event: React.FormEvent) {
+		event.preventDefault();
+		console.log("transfer requested");
+	}
+
+	const handleChange = (value: string) => setAmount(value);
+
+	return (
+		<form onSubmit={submitTransfer}>
+			<FormControl>
+				<FormLabel htmlFor="amount">Amount: </FormLabel>
+				<NumberInput
+					defaultValue={amount}
+					min={15}
+					max={100}
+					onChange={handleChange}
+				>
+					<NumberInputField />
+				</NumberInput>
+				<FormLabel htmlFor="toaddress">To Address: </FormLabel>
+				<Input
+					id="toaddress"
+					type="text"
+					required
+					onChange={(e) => setToAddress(e.target.value)}
+					my={2}
+				/>
+				<Button type="submit" isDisabled={!currentAccount}>
+					Get Coin
+				</Button>
+			</FormControl>
+		</form>
+	);
+}
